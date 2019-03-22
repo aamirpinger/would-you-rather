f98d254c5667490d202fedd04d9754f3e40a9a6c
aamirpinger <aamirpinger@yahoo.com>
2019-03-22 15:11:32 +0500

    Error page add

---
+++ src/components/ErrorPage.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import Typography from '@material-ui/core/Typography';
+
+export default function ErrorPage() {
+
+    return (
+        <div>
+            <Typography
+                variant="h2"
+                noWrap>
+                Page not fount
+            </Typography>
+        </div>
+    )
+}
